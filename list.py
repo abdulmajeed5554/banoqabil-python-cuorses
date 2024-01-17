ee7a80b74a04d439abe4e87907302833ae64626c
abdulmajeed5554 <156215246+abdulmajeed5554@users.noreply.github.com>
2024-01-17 21:49:38 +0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,14 @@
+# abdul majeed
+# amjeed5554@gmail.com    03120090135
+
+# task 1
+# Write a Python program to do arithmetical operations addition and division.
+num1=5
+num2=6
+sum=num1+num2
+print(sum)
+
+num1=25
+num2=5
+division=num1/num2
+print(division)
